7b615de9bf8a76682f5195c70a627fae0d3ee53a
eevan7a9 <e7a9@protonmail.com>
2019-08-02 14:10:41 +0900

    added concatonation samples

---
+++ concat.py
@@ -0,0 +1,15 @@
+# declare variable
+hero_name = "saitama"
+hero_nickname = "caped baldy"
+hero_age = 24
+
+# concatonate two string
+this_hero = hero_name + " the " + hero_nickname
+# concatonate two string with  quote
+hero_said = f"{hero_name} said \"he's too Strong\""
+# concatonate string with int
+hero_who = f"{hero_name} is {str(hero_age)}"
+# print hero
+print(this_hero)
+print(hero_said)
+print(hero_who)
