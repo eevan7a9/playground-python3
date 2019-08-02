7a7c2591b1dc2dd276c056aa32075ef55db6d27d
eevan7a9 <e7a9@protonmail.com>
2019-08-02 13:43:54 +0900

    variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# string varialbe fruit with value 'apple'
+fruit = "apple"
+# integer  variable age with value '23'
+age = 23
+# float variable money with value '23.4'
+money = 23.4
+# boolean variable true or false
+is_happy = True
+is_sad = False
+
+# print variables
+print(fruit)
+print(age)
+print(money)
+print(is_happy, is_sad)
